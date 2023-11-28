c349d419e7432bc640734ec866201ab49bc1c5cf
miloszwierucki <milosz@wierucki.com>
2023-11-28 18:22:47 +0100

    Create Navbar components

---
+++ app/components/BurgerMenu.tsx
@@ -0,0 +1,32 @@
+export interface BurgerMenuProps {
+  style: string;
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const BurgerMenu = ({ open, setOpen, style }: BurgerMenuProps) => {
+  return (
+    <div className={`z-10 items-center ${style}`}>
+      <button
+        className="w-8 h-8 flex flex-col justify-center items-center"
+        onClick={() => setOpen(open ? false : true)}
+      >
+        <span
+          className={`block absolute h-[1px] w-7 bg-primary transform transition duration-500 ease-in-out dark:bg-darkprimary ${
+            open ? "rotate-45" : "translate-y-2"
+          }`}
+        ></span>
+        <span
+          className={`block absolute h-[1px] w-7 bg-primary transform transition duration-300 ease-in-out dark:bg-darkprimary ${
+            open ? "opacity-0" : ""
+          }`}
+        ></span>
+        <span
+          className={`block absolute h-[1px] w-7 bg-primary transform transition duration-500 ease-in-out dark:bg-darkprimary ${
+            open ? "-rotate-45" : "-translate-y-2"
+          }`}
+        ></span>
+      </button>
+    </div>
+  );
+};
---
+++ app/components/ThemeButton.tsx
@@ -0,0 +1,22 @@
+import { useTheme } from "next-themes";
+
+const ThemeButton = () => {
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  return (
+    <div className="relative inline-flex items-center cursor-pointer">
+      <button
+        onClick={() => (theme == "dark" ? setTheme("light") : setTheme("dark"))}
+      >
+        <div
+          className={`relative w-14 h-5 rounded-full border-solid border-[1px] border-primary after:absolute after:h-4 after:w-4 after:content-[''] after:top-[0.075rem] after:left-[0.075rem] after:rounded-full after:bg-primary after:transition-all after:ease-in-out after:duration-300 lg:h-6 lg:after:h-[1.125rem] lg:after:w-[1.125rem] lg:after:top-0.5 lg:after:left-0.5 dark:border-darkprimary dark:after:bg-darkprimary ${
+            theme === "dark" && "after:translate-x-8"
+          }`}
+        ></div>
+      </button>
+    </div>
+  );
+};
+
+export default ThemeButton;
