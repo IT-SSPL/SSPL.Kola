71a572e4b86e1e715cd32dd342c98de25c8637d2
Michał Żelazko <62725023+MichalZelazko@users.noreply.github.com>
2023-12-03 03:33:41 +0100

    Faq page (#5)

---
+++ app/faq/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSuspenseQuery } from "@apollo/client";
+import { FETCH_FAQ_QUESTIONS } from "@/app/lib/fetchFaqQuestions";
+import { Suspense } from "react";
+import ContentContainer from "../components/ContentContainer";
+import Header from "../components/Header";
+
+const Page = () => {
+  const { data, error } = useSuspenseQuery(FETCH_FAQ_QUESTIONS);
+
+  if (error) return <p>Error :\</p>; // TODO: Replace with error page
+
+  return (
+    <main className="container flex flex-grow flex-col mx-auto items-center justify-between py-4 md:py-8 lg:py-12 xl:py-16">
+      <div className="flex flex-col gap-10 w-full ">
+        <div className="flex flex-col gap-2">
+          <Header title={"FAQ"} subtitle={"czyli pytania i odpowiedzi"} small />
+        </div>
+        <ContentContainer style={"flex flex-col gap-3"}>
+          <Suspense fallback={<div>Loading...</div>}>
+            {data.faqQuestions.data.map(({ attributes }) => (
+              <details key={attributes.question}>
+                <summary className="font-bold">{attributes.question}</summary>
+                <p className="pt-5 pb-7">{attributes.answer}</p>
+              </details>
+            ))}
+          </Suspense>
+        </ContentContainer>
+      </div>
+    </main>
+  );
+};
+
+export default Page;
