24aa21d43d725442cd4f2974c5edf47c1e0b337c
Michał Żelazko <242149@edu.p.lodz.pl>
2023-12-03 02:15:38 +0100

    Add ContactCard component

---
+++ app/components/ContactCard.tsx
@@ -0,0 +1,40 @@
+import { HiArrowLeft } from "react-icons/hi2";
+import { IContact } from "../lib/definitions";
+import { Suspense } from "react";
+import ContentContainer from "./ContentContainer";
+
+const ContactCard = ({ email, address }: IContact) => {
+  return (
+    <ContentContainer style={"flex-col md:flex-row justify-between group"}>
+      <div className="flex text-lg lg:text-xl flex-col gap-2">
+        <Suspense fallback={<div>Loading...</div>}>
+          <div className="flex flex-col md:flex-row gap-1 md:gap-2">
+            <h3 className="font-extrabold md:text-right">E-mail:</h3>
+            <a href={`mailto:${email}`}>{email}</a>
+          </div>
+          <div className="flex flex-col md:flex-row gap-1 md:gap-2">
+            <h3 className="font-extrabold md:text-right">Adres:</h3>
+            <div className="flex flex-col gap-1">
+              <p>
+                {address.building}{" "}
+                {address.room && <span>/{address.room}</span>}
+              </p>
+              <p>Kampus {address.campus}</p>
+              <p>{address.street}</p>
+            </div>
+          </div>
+        </Suspense>
+      </div>
+      <div className="hidden md:flex font-semibold">
+        <div className="group flex items-center gap-5">
+          <p className="relative text-3xl transition-all duration-300 group-hover:-translate-x-10">
+            <HiArrowLeft />
+          </p>
+          <p className="text-xl">KONTAKT</p>
+        </div>
+      </div>
+    </ContentContainer>
+  );
+};
+
+export default ContactCard;
--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -5,7 +5,7 @@ import { FETCH_CONTACT } from "../lib/fetchContact";
 import { Suspense } from "react";
 import { HiArrowLeft } from "react-icons/hi2";
 import Header from "../components/Header";
-import ContentContainer from "../components/ContentContainer";
+import ContactCard from "../components/ContactCard";
 
 const Page = () => {
   const { data, error } = useSuspenseQuery(FETCH_CONTACT);
@@ -20,39 +20,10 @@ const Page = () => {
             small
           />
         </div>
-        <ContentContainer style={"flex-col md:flex-row justify-between group"}>
-          <div className="flex text-lg lg:text-xl flex-col gap-2">
-            <Suspense fallback={<div>Loading...</div>}>
-              <div className="flex flex-col md:flex-row gap-1 md:gap-2">
-                <h3 className="font-extrabold md:text-right">E-mail:</h3>
-                <a href={`mailto:${data.contact.data.attributes.email}`}>
-                  {data.contact.data.attributes.email}
-                </a>
-              </div>
-              <div className="flex flex-col md:flex-row gap-1 md:gap-2">
-                <h3 className="font-extrabold md:text-right">Adres:</h3>
-                <div className="flex flex-col gap-1">
-                  <p>
-                    {data.contact.data.attributes.address.building}{" "}
-                    {data.contact.data.attributes.address.room && (
-                      <span>/{data.contact.data.attributes.address.room}</span>
-                    )}
-                  </p>
-                  <p>Kampus {data.contact.data.attributes.address.campus}</p>
-                  <p>{data.contact.data.attributes.address.street}</p>
-                </div>
-              </div>
-            </Suspense>
-          </div>
-          <div className="hidden md:flex font-semibold">
-            <div className="group flex items-center gap-5">
-              <p className="relative text-3xl transition-all duration-300 group-hover:-translate-x-10">
-                <HiArrowLeft />
-              </p>
-              <p className="text-xl">KONTAKT</p>
-            </div>
-          </div>
-        </ContentContainer>
+        <ContactCard
+          email={data.contact.data.attributes.email}
+          address={data.contact.data.attributes.address}
+        />
       </div>
     </main>
   );
