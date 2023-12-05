556c5dbe5b22d3524861bc90574d0c3597b45929
miloszwierucki <milosz@wierucki.com>
2023-12-05 01:26:20 +0100

    Hotfix: Theme in localStorage

--- app/components/ThemeButton.tsx
+++ app/components/ThemeButton.tsx
@@ -8,7 +8,7 @@ const ThemeButton = () => {
   useEffect(() => {
     setMounted(true);
 
-    if (systemTheme) {
+    if (systemTheme && theme === "system") {
       setTheme(systemTheme);
     }
   }, [setTheme, systemTheme]);
