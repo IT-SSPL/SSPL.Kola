2937b27d368e045d52e1b0b87a423dc382bf0643
Michał Żelazko <242149@edu.p.lodz.pl>
2024-03-06 01:08:59 +0100

    Carousel works, needs better styling

--- app/components/AchievementCard.tsx
+++ app/components/AchievementCard.tsx
@@ -3,11 +3,12 @@ import ContentContainer from "./ContentContainer";
 export interface IAchievement {
   title: string;
   description: string;
+  style?: React.CSSProperties;
 }
 
-const AchievementCard = ({ title, description }: IAchievement) => {
+const AchievementCard = ({ title, description, style }: IAchievement) => {
   return (
-    <ContentContainer style={"flex-col md:flex-row justify-between"}>
+    <ContentContainer style={`flex-col md:flex-row justify-between ${style}`}>
       <div className="flex flex-col gap-2">
         <h3 className="font-extrabold">{title}</h3>
         <p>{description}</p>
--- app/components/AchievementsCarousel.tsx
+++ app/components/AchievementsCarousel.tsx
@@ -1,40 +1,110 @@
-import React from "react";
-import Carousel from "react-spring-3d-carousel";
-import { config } from "react-spring";
+"use client";
+
+// import React, { useState } from "react";
+// import AchievementCard, { IAchievement } from "./AchievementCard";
+// import { HiArrowLeft, HiArrowRight } from "react-icons/hi2";
+// import "./Carousel.css";
+
+// interface AchievementsCarouselProps {
+//   achievements: IAchievement[];
+// }
+
+// const AchievementsCarousel = ({ achievements }: AchievementsCarouselProps) => {
+//   const [active, setActive] = useState(2);
+
+//   const MAX_VISIBILITY = 3;
+
+//   return (
+//     <div className="carousel">
+//       {active > 0 && (
+//         <button className="nav left" onClick={() => setActive((i) => i - 1)}>
+//           <HiArrowLeft />
+//         </button>
+//       )}
+//       {achievements.map((achievement, i) => (
+//         <AchievementCard
+//           key={i}
+//           style={{
+//             "--active": i === active ? 1 : 0,
+//             "--offset": `${(active - i) / 3}`,
+//             "--direction": `${Math.sign(active - i)}`,
+//             "--abs-offset": `${Math.abs(active - i) / 3}`,
+//             pointerEvents: active === i ? "auto" : "none",
+//             opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+//             display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+//           }}
+//           title={achievement.title}
+//           description={achievement.description}
+//         />
+//       ))}
+//       {active < achievements.length - 1 && (
+//         <button className="nav right" onClick={() => setActive((i) => i + 1)}>
+//           <HiArrowRight />
+//         </button>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default AchievementsCarousel;
+
+import React, { CSSProperties, useState } from "react";
+import { HiArrowLeft, HiArrowRight } from "react-icons/hi2";
 import AchievementCard, { IAchievement } from "./AchievementCard";
+import "./Carousel.css";
+
+const MAX_VISIBILITY = 3;
 
 interface AchievementsCarouselProps {
   achievements: IAchievement[];
 }
 
-// const getTouches = (evt: Event) => {
-//   return (
-//     evt.touches || evt.originalEvent.touches // browser API
-//   );
-// };
+const Card = ({ title, description }: IAchievement) => (
+  <div className="card">
+    <h2>{title}</h2>
+    <p>{description}</p>
+  </div>
+);
 
-const AchievementsCarousel = ({ achievements }: AchievementsCarouselProps) => {
-  const slides = achievements.map((achievement, index) => {
-    return {
-      key: index,
-      content: (
-        <AchievementCard
-          title={achievement.title}
-          description={achievement.description}
-        />
-      ),
-    };
-  });
+const AchievementCarousel = ({ achievements }: AchievementsCarouselProps) => {
+  const [active, setActive] = useState(1);
 
   return (
-    <Carousel
-      slides={slides}
-      goToSlide={0}
-      offsetRadius={2}
-      showNavigation={true}
-      animationConfig={config.gentle}
-    />
+    <div className="carousel">
+      {active > 0 && (
+        <button className="nav left" onClick={() => setActive((i) => i - 1)}>
+          <HiArrowLeft />
+        </button>
+      )}
+      {achievements.map((achievement, i) => (
+        <div
+          className="card-container"
+          style={{
+            ...({
+              "--active": i === active ? 1 : 0,
+              "--offset": (active - i) / 3,
+              "--direction": Math.sign(active - i),
+              "--abs-offset": Math.abs(active - i) / 3,
+              "pointer-events": active === i ? "auto" : "none",
+            } as CSSProperties),
+            opacity: Math.abs(active - i) >= MAX_VISIBILITY ? "0" : "1",
+            display: Math.abs(active - i) > MAX_VISIBILITY ? "none" : "block",
+          }}
+          key={i}
+        >
+          <AchievementCard
+            title={achievement.title}
+            description={achievement.description}
+          />
+        </div>
+      ))}
+      {active < achievements.length - 1 && (
+        <button className="nav right" onClick={() => setActive((i) => i + 1)}>
+          <HiArrowRight />
+        </button>
+      )}
+    </div>
   );
 };
 
-export default AchievementsCarousel;
+export default AchievementCarousel;
